03b2f3d0a3e2c4f1493f13282bac01931289e4c2
Josh Jacoby <joshuadjacoby@gmail.com>
2019-10-15 19:13:20 +0000

    Updates

---
+++ precache-manifest.2d885e1efbe41e83f5b5515f31de5737.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70065a9e87ae969afbc89554572e1b4a",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "e4b88eaa98dc69012f26",
+    "url": "/react-rolodex/static/js/2.76f2738e.chunk.js"
+  },
+  {
+    "revision": "78db19102c53bc197312",
+    "url": "/react-rolodex/static/js/main.ca167489.chunk.js"
+  },
+  {
+    "revision": "eeab36ac02304cff96dd",
+    "url": "/react-rolodex/static/js/runtime-main.f9f1c59a.js"
+  }
+]);
